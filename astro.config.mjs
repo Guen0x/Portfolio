c6d05d144948aaa0425b65ce05f42b65b057cf25
guenox <stephane.guenounoul@gmail.com>
2024-10-17 18:36:42 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://guen0x.github.io',
+    base: 'Portfolio'
+}); 
+
