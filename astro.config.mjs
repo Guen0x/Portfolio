fff419d6d0e66c526e05c4c735f2c8572a642a30
guenox <stephane.guenounoul@gmail.com>
2024-10-17 18:41:19 +0200

    Modif

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://guen0x.github.io',
-    base: 'Portfolio'
+    site: 'https://guen0x.github.io/Portfolio',
 }); 
 
